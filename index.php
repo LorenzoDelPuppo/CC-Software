b32c4429ef1519ff11af9cff185504c53c5a1b41
tommaso-poletto <tommaso.poletto@iisvittorioveneto.it>
2024-12-08 16:45:51 +0100

    commenti registrazione e e html(no pulsanti) - funzionante

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Una breve descrizione del contenuto della pagina.">
+    <meta name="description" content="descrizione pagina">
     <meta name="keywords" content="HTML, CSS, JavaScript, Tutorial">
-    <meta name="author" content="Il tuo nome o nome dell'azienda">
+    <meta name="author" content="CC-Softwer">
     <title>Che Capelli</title>
     <script src="app.js"></script>
     <link rel="stylesheet" href="style.css">
@@ -19,6 +19,7 @@
     <br>
     <div class="form-container">
         <form action="index.php" method="post">
+              <!-- Campo di input per i dati -->
             <label for="fName">Nome</label>
             <input type="text" id="fName" name="fName" placeholder="Inserisci" required>
             
@@ -33,7 +34,8 @@
         
             <label for="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Inserisci" required>
- 
+            
+            <!-- Menu a tendina per selezionare il genere -->
             <div class="dropdown">
                 <label for="gender">Genere</label>
                 <select id="gender" name="gender">
@@ -70,9 +72,11 @@
 
 <?php
 require_once 'connect.php';
+//richiesta http post
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
+    //crazione delle variabili per memorizzarele con il post
     $firstName = $_POST['fName'];
     $lastName = $_POST['sName'];
     $phoneNumber = $_POST['phoneN'];
@@ -81,9 +85,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $hairType = $_POST['lunghezzaCapelli'];
     $gender = $_POST['gender'];
 
+    //query inserimento
     $sql = "INSERT INTO customer (fName, lName, phoneN, email, password, hair, gender)
             VALUES (?,?,?,?,?,?,?)";
 
+    //connessione 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('sssssss', $firstName,$lastName,$phoneNumber,$email,$password,$hairType,$gender);
 
