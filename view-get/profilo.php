bb572210423f5ce0b0be1290b2624d439552818a
Al3-Morett0n <alessandro.moretton@iisvittorioveneto.it>
2025-02-17 12:59:35 +0100

    aggiunta icona su menu a tendina

--- view-get/profilo.php
+++ view-get/profilo.php
@@ -42,20 +42,31 @@ $conn->close();
   </div>
 
   <div class="right-section">
-  <div class="user-menu">
-  <!-- Icona utente (o un'immagine) -->
-  <span class="user-icon">&#128100;</span>
-  
-  <!-- Dropdown -->
-  <div class="dropdown-menu">
-    <a href=".././view-get/profilo.php" class="dropdown-item">Profilo</a>
-    <a href=".././add-edit/impostazioni.php" class="dropdown-item">Impostazioni</a>
-    <hr class="dropdown-separator">
-    <a href=".././add-edit/logout.php" class="dropdown-item logout-item">Logout</a>
+    <div class="user-menu">
+      <!-- Icona utente (o un'immagine) -->
+      <span class="user-icon">&#128100;</span>
+
+      <!-- Dropdown -->
+      <div class="dropdown-menu">
+        <a href=".././view-get/profilo.php" class="dropdown-item">
+        <img src=".././style/rullino/profilo.svg" alt="Logout Icon" class="logout-icon">
+        Profilo
+        <a href=".././add-edit/impostazioni.php" class="dropdown-item">
+        <img src=".././style/rullino/imp.svg" alt="Logout Icon" class="logout-icon">
+        Impostastazioni
+        <br>
+        <hr class="dropdown-separator">
+        
+        <!-- Logout con icona e testo sulla stessa riga -->
+        <a href=".././add-edit/logout.php" class="dropdown-item logout-item">
+          <img src=".././style/rullino/logoutr.svg" alt="Logout Icon" class="logout-icon">
+          Logout
+        </a>
+      </div>
+    </div>
   </div>
 </div>
-</div>
-</div>
+
 
 
 <head>
