c22a1a03ca134ed173d299209620cf7854a9c34e
LorenzoDelPuppo <lorenzo.delpuppo@iisvittorioveneto.it>
2025-04-28 12:14:42 +0200

    cancella appuntamenti FUNZIONAAAAA

--- add-edit/cancella_appuntamenti.php
+++ add-edit/cancella_appuntamenti.php
@@ -1,23 +1,25 @@
 <?php
-// Connessione al database, collegandosi al file 
+// Connessione al database
 require_once __DIR__ . '/../connect.php';
 
 // Ottieni la data e ora attuale
 $currentDateTime = date("Y-m-d H:i:s");
+//echo "Data e ora corrente: " . $currentDateTime . "<br>"; // Debug per la data corrente
 
 // Query per selezionare gli appuntamenti passati
 $sql = "DELETE FROM appointment WHERE dateTime < ?";
+//echo "Query: " . $sql . " | DateTime: " . $currentDateTime . "<br>"; // Debug della query
+
 if ($stmt = $conn->prepare($sql)) {
     $stmt->bind_param("s", $currentDateTime);
     if ($stmt->execute()) {
-        echo "Gli appuntamenti passati sono stati cancellati con successo.";
+        //echo "Gli appuntamenti passati sono stati cancellati con successo.";
     } else {
-        echo "Errore durante la cancellazione degli appuntamenti: " . $stmt->error;
+        $stmt->error;
     }
     $stmt->close();
 } else {
     echo "Errore nella preparazione della query.";
 }
 
-$conn->close();
 ?>
