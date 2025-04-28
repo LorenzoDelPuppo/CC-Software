7586b6b709459565678e3576da460f54d01d35df
LorenzoDelPuppo <lorenzo.delpuppo@iisvittorioveneto.it>
2025-04-28 12:52:09 +0200

    aggiornamento cancella appunt, lista_clienti e tolto il lavoro di un'ora ;(

--- add-edit/cancella_appuntamenti.php
+++ add-edit/cancella_appuntamenti.php
@@ -1,12 +1,10 @@
 <?php
-// Connessione al database
 require_once __DIR__ . '/../connect.php';
 
-// Ottieni la data e ora attuale
+//prendo la data attuale
 $currentDateTime = date("Y-m-d H:i:s");
 //echo "Data e ora corrente: " . $currentDateTime . "<br>"; // Debug per la data corrente
 
-// Query per selezionare gli appuntamenti passati
 $sql = "DELETE FROM appointment WHERE dateTime < ?";
 //echo "Query: " . $sql . " | DateTime: " . $currentDateTime . "<br>"; // Debug della query
 
